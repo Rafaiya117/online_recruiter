d778ac7510bcbedd12c85d8e72a9afff65ac5555
Rafaiya117 <rushdha117@gmail.com>
2021-07-13 12:33:07 +0600

    Initial commit

---
+++ resources/views/master.blade.php
@@ -0,0 +1,238 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <base href="./">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <meta name="description" content="CoreUI - Open Source Bootstrap Admin Template">
+    <meta name="author" content="Łukasz Holeczek">
+    <meta name="keyword" content="Bootstrap,Admin,Template,Open,Source,jQuery,CSS,HTML,RWD,Dashboard">
+    <title>CoreUI Pro Bootstrap Admin Template</title>
+
+    <link href="{{url('/css/style.css')}}" rel="stylesheet">
+    <link href="{{url('/css/coreui-chartjs.css')}}" rel="stylesheet">
+    <meta name="robots" content="noindex">
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-118965717-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'UA-118965717-1');
+    </script></head>
+<body class="c-app c-dark-theme c-no-layout-transition">
+@include('backend.partials.sidebar')
+<div class="c-sidebar c-sidebar-lg c-sidebar-light c-sidebar-right c-sidebar-overlaid" id="aside">
+    <button class="c-sidebar-close c-class-toggler" type="button" data-target="_parent" data-class="c-sidebar-show" responsive="true">
+        <svg class="c-icon">
+            <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-x"></use>
+        </svg>
+    </button>
+    <ul class="nav nav-tabs nav-underline nav-underline-primary" role="tablist">
+        <li class="nav-item"><a class="nav-link active" data-toggle="tab" href="#timeline" role="tab">
+                <svg class="c-icon">
+                    <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-list"></use>
+                </svg></a></li>
+        <li class="nav-item"><a class="nav-link" data-toggle="tab" href="#messages" role="tab">
+                <svg class="c-icon">
+                    <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-speech"></use>
+                </svg></a></li>
+        <li class="nav-item"><a class="nav-link" data-toggle="tab" href="#settings" role="tab">
+                <svg class="c-icon">
+                    <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-settings"></use>
+                </svg></a></li>
+    </ul>
+
+    <div class="tab-content">
+        <div class="tab-pane active" id="timeline" role="tabpanel">
+            <div class="list-group list-group-accent">
+                <div class="list-group-item list-group-item-accent-secondary bg-light text-center font-weight-bold text-muted text-uppercase c-small">Today</div>
+                <div class="list-group-item list-group-item-accent-warning list-group-item-divider">
+                    <div class="c-avatar float-right"><img class="c-avatar-img" src="assets/img/avatars/7.jpg" alt="user@email.com"></div>
+                    <div>Meeting with <strong>Lucas</strong></div><small class="text-muted mr-3">
+                        <svg class="c-icon">
+                            <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-calendar"></use>
+                        </svg>&nbsp; 1 - 3pm</small><small class="text-muted">
+                        <svg class="c-icon">
+                            <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-location-pin"></use>
+                        </svg>&nbsp; Palo Alto, CA</small>
+                </div>
+                <div class="list-group-item list-group-item-accent-info">
+                    <div class="c-avatar float-right"><img class="c-avatar-img" src="assets/img/avatars/4.jpg" alt="user@email.com"></div>
+                    <div>Skype with <strong>Megan</strong></div><small class="text-muted mr-3">
+                        <svg class="c-icon">
+                            <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-calendar"></use>
+                        </svg>&nbsp; 4 - 5pm</small><small class="text-muted">
+                        <svg class="c-icon">
+                            <use xlink:href="vendors/@coreui/icons/svg/brand.svg#cib-skype"></use>
+                        </svg>&nbsp; On-line</small>
+                </div>
+                <div class="list-group-item list-group-item-accent-secondary bg-light text-center font-weight-bold text-muted text-uppercase c-small">Tomorrow</div>
+                <div class="list-group-item list-group-item-accent-danger list-group-item-divider">
+                    <div>New UI Project - <strong>deadline</strong></div><small class="text-muted mr-3">
+                        <svg class="c-icon">
+                            <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-calendar"></use>
+                        </svg>&nbsp; 10 - 11pm</small><small class="text-muted">
+                        <svg class="c-icon">
+                            <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-home"></use>
+                        </svg>&nbsp; creativeLabs HQ</small>
+                    <div class="c-avatars-stack mt-2">
+                        <div class="c-avatar c-avatar-xs"><img class="c-avatar-img" src="assets/img/avatars/2.jpg" alt="user@email.com"></div>
+                        <div class="c-avatar c-avatar-xs"><img class="c-avatar-img" src="assets/img/avatars/3.jpg" alt="user@email.com"></div>
+                        <div class="c-avatar c-avatar-xs"><img class="c-avatar-img" src="assets/img/avatars/4.jpg" alt="user@email.com"></div>
+                        <div class="c-avatar c-avatar-xs"><img class="c-avatar-img" src="assets/img/avatars/5.jpg" alt="user@email.com"></div>
+                        <div class="c-avatar c-avatar-xs"><img class="c-avatar-img" src="assets/img/avatars/6.jpg" alt="user@email.com"></div>
+                    </div>
+                </div>
+                <div class="list-group-item list-group-item-accent-success list-group-item-divider">
+                    <div><strong>#10 Startups.Garden</strong> Meetup</div><small class="text-muted mr-3">
+                        <svg class="c-icon">
+                            <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-calendar"></use>
+                        </svg>&nbsp; 1 - 3pm</small><small class="text-muted">
+                        <svg class="c-icon">
+                            <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-location-pin"></use>
+                        </svg>&nbsp; Palo Alto, CA</small>
+                </div>
+                <div class="list-group-item list-group-item-accent-primary list-group-item-divider">
+                    <div><strong>Team meeting</strong></div><small class="text-muted mr-3">
+                        <svg class="c-icon">
+                            <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-calendar"></use>
+                        </svg>&nbsp; 4 - 6pm</small><small class="text-muted">
+                        <svg class="c-icon">
+                            <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-home"></use>
+                        </svg>&nbsp; creativeLabs HQ</small>
+                    <div class="c-avatars-stack mt-2">
+                        <div class="c-avatar c-avatar-xs"><img class="c-avatar-img" src="assets/img/avatars/2.jpg" alt="user@email.com"></div>
+                        <div class="c-avatar c-avatar-xs"><img class="c-avatar-img" src="assets/img/avatars/3.jpg" alt="user@email.com"></div>
+                        <div class="c-avatar c-avatar-xs"><img class="c-avatar-img" src="assets/img/avatars/4.jpg" alt="user@email.com"></div>
+                        <div class="c-avatar c-avatar-xs"><img class="c-avatar-img" src="assets/img/avatars/5.jpg" alt="user@email.com"></div>
+                        <div class="c-avatar c-avatar-xs"><img class="c-avatar-img" src="assets/img/avatars/6.jpg" alt="user@email.com"></div>
+                        <div class="c-avatar c-avatar-xs"><img class="c-avatar-img" src="assets/img/avatars/7.jpg" alt="user@email.com"></div>
+                        <div class="c-avatar c-avatar-xs"><img class="c-avatar-img" src="assets/img/avatars/8.jpg" alt="user@email.com"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="tab-pane p-3" id="messages" role="tabpanel">
+            <div class="message">
+                <div class="py-3 pb-5 mr-3 float-left">
+                    <div class="c-avatar"><img class="c-avatar-img" src="assets/img/avatars/7.jpg" alt="user@email.com"><span class="c-avatar-status bg-success"></span></div>
+                </div>
+                <div><small class="text-muted">Lukasz Holeczek</small><small class="text-muted float-right mt-1">1:52 PM</small></div>
+                <div class="text-truncate font-weight-bold">Lorem ipsum dolor sit amet</div><small class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt...</small>
+            </div>
+            <hr>
+            <div class="message">
+                <div class="py-3 pb-5 mr-3 float-left">
+                    <div class="c-avatar"><img class="c-avatar-img" src="assets/img/avatars/7.jpg" alt="user@email.com"><span class="c-avatar-status bg-success"></span></div>
+                </div>
+                <div><small class="text-muted">Lukasz Holeczek</small><small class="text-muted float-right mt-1">1:52 PM</small></div>
+                <div class="text-truncate font-weight-bold">Lorem ipsum dolor sit amet</div><small class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt...</small>
+            </div>
+            <hr>
+            <div class="message">
+                <div class="py-3 pb-5 mr-3 float-left">
+                    <div class="c-avatar"><img class="c-avatar-img" src="assets/img/avatars/7.jpg" alt="user@email.com"><span class="c-avatar-status bg-success"></span></div>
+                </div>
+                <div><small class="text-muted">Lukasz Holeczek</small><small class="text-muted float-right mt-1">1:52 PM</small></div>
+                <div class="text-truncate font-weight-bold">Lorem ipsum dolor sit amet</div><small class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt...</small>
+            </div>
+            <hr>
+            <div class="message">
+                <div class="py-3 pb-5 mr-3 float-left">
+                    <div class="c-avatar"><img class="c-avatar-img" src="assets/img/avatars/7.jpg" alt="user@email.com"><span class="c-avatar-status bg-success"></span></div>
+                </div>
+                <div><small class="text-muted">Lukasz Holeczek</small><small class="text-muted float-right mt-1">1:52 PM</small></div>
+                <div class="text-truncate font-weight-bold">Lorem ipsum dolor sit amet</div><small class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt...</small>
+            </div>
+            <hr>
+            <div class="message">
+                <div class="py-3 pb-5 mr-3 float-left">
+                    <div class="c-avatar"><img class="c-avatar-img" src="assets/img/avatars/7.jpg" alt="user@email.com"><span class="c-avatar-status bg-success"></span></div>
+                </div>
+                <div><small class="text-muted">Lukasz Holeczek</small><small class="text-muted float-right mt-1">1:52 PM</small></div>
+                <div class="text-truncate font-weight-bold">Lorem ipsum dolor sit amet</div><small class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt...</small>
+            </div>
+        </div>
+        <div class="tab-pane p-3" id="settings" role="tabpanel">
+            <h6>Settings</h6>
+            <div class="c-aside-options">
+                <div class="clearfix mt-4"><small><b>Option 1</b></small>
+                    <label class="c-switch c-switch-label c-switch-pill c-switch-success c-switch-sm float-right">
+                        <input class="c-switch-input" type="checkbox" checked=""><span class="c-switch-slider" data-checked="On" data-unchecked="Off"></span>
+                    </label>
+                </div>
+                <div><small class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</small></div>
+            </div>
+            <div class="c-aside-options">
+                <div class="clearfix mt-3"><small><b>Option 2</b></small>
+                    <label class="c-switch c-switch-label c-switch-pill c-switch-success c-switch-sm float-right">
+                        <input class="c-switch-input" type="checkbox"><span class="c-switch-slider" data-checked="On" data-unchecked="Off"></span>
+                    </label>
+                </div>
+                <div><small class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</small></div>
+            </div>
+            <div class="c-aside-options">
+                <div class="clearfix mt-3"><small><b>Option 3</b></small>
+                    <label class="c-switch c-switch-label c-switch-pill c-switch-success c-switch-sm float-right">
+                        <input class="c-switch-input" type="checkbox"><span class="c-switch-slider" data-checked="On" data-unchecked="Off"></span>
+                    </label>
+                </div>
+            </div>
+            <div class="c-aside-options">
+                <div class="clearfix mt-3"><small><b>Option 4</b></small>
+                    <label class="c-switch c-switch-label c-switch-pill c-switch-success c-switch-sm float-right">
+                        <input class="c-switch-input" type="checkbox" checked=""><span class="c-switch-slider" data-checked="On" data-unchecked="Off"></span>
+                    </label>
+                </div>
+            </div>
+            <hr>
+            <h6>System Utilization</h6>
+            <div class="text-uppercase mb-1 mt-4"><small><b>CPU Usage</b></small></div>
+            <div class="progress progress-xs">
+                <div class="progress-bar bg-info" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+            </div><small class="text-muted">348 Processes. 1/4 Cores.</small>
+            <div class="text-uppercase mb-1 mt-2"><small><b>Memory Usage</b></small></div>
+            <div class="progress progress-xs">
+                <div class="progress-bar bg-warning" role="progressbar" style="width: 70%" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100"></div>
+            </div><small class="text-muted">11444GB/16384MB</small>
+            <div class="text-uppercase mb-1 mt-2"><small><b>SSD 1 Usage</b></small></div>
+            <div class="progress progress-xs">
+                <div class="progress-bar bg-danger" role="progressbar" style="width: 95%" aria-valuenow="95" aria-valuemin="0" aria-valuemax="100"></div>
+            </div><small class="text-muted">243GB/256GB</small>
+            <div class="text-uppercase mb-1 mt-2"><small><b>SSD 2 Usage</b></small></div>
+            <div class="progress progress-xs">
+                <div class="progress-bar bg-success" role="progressbar" style="width: 10%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div>
+            </div><small class="text-muted">25GB/256GB</small>
+        </div>
+    </div>
+</div>
+<div class="c-wrapper">
+   @include('backend.partials.header')
+    <div class="c-body">
+       @include('backend.partials.main')
+    </div>
+    <footer class="c-footer">
+        <div><a href="https://coreui.io">CoreUI</a> © 2020 creativeLabs.</div>
+        <div class="mfs-auto">Powered by&nbsp;<a href="https://coreui.io/pro/">CoreUI Pro</a></div>
+    </footer>
+</div>
+
+<script src="{{url('/js/coreui.bundle.min.js')}}"></script>
+<!--[if IE]><!-->
+<script src="{{url('/js/svgxuse.min.js')}}"></script>
+<!--<![endif]-->
+
+<script src="{{url('/js/coreui-chartjs.bundle.js')}}"></script>
+<script src="{{url('/js/coreui-utils.js')}}"></script>
+<script src="{{url('/js/main.js')}}"></script>
+<script>
+    document.addEventListener("DOMContentLoaded", function(event) {
+        setTimeout(function() {
+            document.body.classList.remove('c-no-layout-transition')
+        }, 2000);
+    });
+</script>
+</body>
+</html>
