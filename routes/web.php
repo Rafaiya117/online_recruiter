8395422a4269eef2063ee929384a0044cf1516a8
Rafaiya117 <rushdha117@gmail.com>
2021-07-19 20:37:04 +0600

    first commit

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,4 +15,6 @@ use App\Http\Controllers\HomeController;
 |
 */
 
+
 Route::get('/',[HomeController::class,'home']);
+
